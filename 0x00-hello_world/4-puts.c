c1aac6ce5ab88709b4f55a0a6aa3e3f7c5f6dcfb
khaledalgoker <khaledhanafy0002@gmail.com>
2023-03-16 14:34:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entey point
- *
- *Description: A C program that prints with put function
- *
- * Return: Always 0 (Success)
+* main - Entey point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
